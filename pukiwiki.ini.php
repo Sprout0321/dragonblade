9810ab7ef3d4dc41fe81a3eeaad7601b663fcdfa
Sprout0321 <mel20564@gmail.com>
2017-05-22 18:18:14 +0900

    add point to sp

--- pukiwiki.ini.php
+++ pukiwiki.ini.php
@@ -476,7 +476,7 @@ $agents = array(
     // Embedded browsers (Non-rich)
 
     // スマートフォン判定追記
-	array('pattern'=>'#\b(Android|iPhone)\b#', 'profile'=>'keitai'),
+	array('pattern'=>'#\b(Android|iPhone)\b#', 'profile'=>'sp'),
 
 	// Windows CE (the others)
 	// Sample: "Mozilla/2.0 (compatible; MSIE 3.02; Windows CE; 240x320 )" (GFORT, NTT DoCoMo)
