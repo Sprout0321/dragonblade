e57ab982b0eaf82c09107439491c898bfb727ec6
Sprout Kuo <Sprout0321@kuruwakashiwashouteki-MacBook-Air.local>
2017-05-25 11:57:29 +0900

    fit image

--- skin/pukiwiki.css.php
+++ skin/pukiwiki.css.php
@@ -330,7 +330,7 @@ strong.word9 {
 
 /* pukiwiki.skin.php */
 div#header {
-	<!-- height:200px; -->
+	height:200px;
 	padding:0px;
 	margin:0px;
 	background-image:url( ' http://wiki.dragon-blade.jp/image/header.jpeg ' );
